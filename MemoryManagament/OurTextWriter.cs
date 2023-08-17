a8243c4586b142da45d68228a0b405d19fd44a1d
Almir Vuk <almir.vuk@outlook.com>
2023-08-17 20:26:02 +0200

    Class 12 code added.

---
+++ MemoryManagament/OurTextWriter.cs
@@ -0,0 +1,35 @@
+﻿namespace MemoryManagament
+{
+    public class OurTextWriter : IDisposable
+    {
+        private string _path;
+        private bool _disposedValue = false;
+
+        private StreamWriter _streamWriter;
+
+        public OurTextWriter(string path)
+        {
+            _path = path;
+
+            _streamWriter = new StreamWriter(path, true);
+        }
+       
+        public void Write(string content)
+        {
+            Console.WriteLine($"We are writing into file at {_path}");
+
+            _streamWriter.WriteLine(content);
+        }
+
+        public void Dispose()
+        {
+            // clear all the resources here
+            _streamWriter?.Dispose();
+            _path = "";
+            _disposedValue = true;
+
+            Console.WriteLine("We are now disposing this class");
+        }
+
+    }
+}
---
+++ MemoryManagament/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Data.SqlTypes;
+
+namespace MemoryManagament
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+
+            student.FirstName = "Zoran";
+            student.LastName = "Malinovic";
+
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+
+            Console.WriteLine("Before: Student student2 = student");
+
+            Student student2 = student;
+            student2.LastName = "MALINOVIC VELIKA SLOVA 2222222";
+
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+            Console.WriteLine($"{student2.FirstName} {student2.LastName}");
+
+            student.FirstName = "YORAN";
+
+            Console.WriteLine("After izmjena");
+
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+            Console.WriteLine($"{student2.FirstName} {student2.LastName}");
+
+            student2 = new Student();
+
+            Console.WriteLine("After: student2 = new Student();");
+
+            student2.FirstName = "Almir";
+            student2.LastName = "Vuk";
+
+            string sadrya = $"{student2.FirstName} {student2.LastName}";
+
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+            Console.WriteLine(sadrya);
+
+            Student student3 = student2;
+
+            student = null;
+            student2 = null;
+
+            student2 = new Student();
+
+            Console.WriteLine("Student3:");
+            Console.WriteLine($"{student3.FirstName} {student3.LastName}");
+
+            Console.WriteLine(".....................................................");
+
+            string path = "novifajl.txt";
+
+            if (File.Exists(path) == false)
+                File.Create(path).Close();
+
+            OurTextWriter ourTextWriter = new OurTextWriter(path);
+
+            using (ourTextWriter)
+            {
+                ourTextWriter.Write("Nova linija");
+                ourTextWriter.Write("Nova linija");
+            }
+        }
+    }
+}
